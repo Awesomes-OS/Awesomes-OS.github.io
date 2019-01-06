cbf26080ce61da3f7fee69c2d31ba3f27a30c540
Andrew <andrew.boyarshin@gmail.com>
2019-01-06 21:25:20 +0700

    Initial commit

---
+++ precache-manifest.6a9509877a7f1fe5ef53990cd37dc27b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "567bf7357eb90741c8b5",
+    "url": "/css/app.d225fc11.css"
+  },
+  {
+    "revision": "567bf7357eb90741c8b5",
+    "url": "/js/app.ba9f05c1.js"
+  },
+  {
+    "revision": "34aad4fd365f90591f8a",
+    "url": "/css/chunk-vendors.69201380.css"
+  },
+  {
+    "revision": "34aad4fd365f90591f8a",
+    "url": "/js/chunk-vendors.2f9bdf4c.js"
+  },
+  {
+    "revision": "acf8b75d0101a5966c4e702be33007fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
